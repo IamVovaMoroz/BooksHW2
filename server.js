c8dc29417e2075b0ed589396813a1ae88d57297f
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-08-19 17:36:10 +0200

    ok

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
 const app = require('./app')
 // для подключения VS code к базе импортруем
-const mongoose = require("mongoose")
-const DB_HOST =  "mongodb+srv://Volodymyr:NOni01041983@cluster0.3fu4wks.mongodb.net/Books_lidrary"
+// const mongoose = require("mongoose")
+// const DB_HOST =  "mongodb+srv://Volodymyr:---NOni01041983@cluster0.3fu4wks.mongodb.net---/Books_lidrary"
 
-mongoose.connect(DB_HOST).then(()=>{ 
-  app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")})
-  console.log("Database connect success")}).catch(
-    error => {
-      console.log(error.message)
-      process.exit(1)
-    })
+// mongoose.connect(DB_HOST).then(()=>{ 
+//   app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")})
+//   console.log("Database connect success")}).catch(
+//     error => {
+//       console.log(error.message)
+//       process.exit(1)
+//     })
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
+app.listen(3000, () => {
+  console.log("Server running. Use our API on port: 3000")
+})
